63172eb3571eec5d6c91cce0177174d3b74ca82d
Jarek Kobus <jaroslaw.kobus@qt.io>
2025-07-17 10:29:32 +0200

    Long live QtTaskTree!
    Fixes: QTBUG-131142
    Change-Id: Ibc3ad65a32988ddd95dc3b9d1221086cad266504
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    Reviewed-by: hjk <hjk@qt.io>
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

---
+++ src/tasktree/qttasktreeglobal.h
@@ -0,0 +1,10 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QTTASKTREEGLOBAL_H
+#define QTTASKTREEGLOBAL_H
+
+#include <QtCore/qglobal.h>
+#include <QtTaskTree/qttasktreeexports.h>
+
+#endif // QTTASKTREEGLOBAL_H
