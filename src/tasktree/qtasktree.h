b2f93223d7184e92ee7efc17c5f164bc0a0841be
Jarek Kobus <jaroslaw.kobus@qt.io>
2026-01-23 09:30:15 +0100

    API review: Simplify variadic capture
    Apparently replacing it with [=] works fine, also in C++17.
    
    Pick-to: 6.11
    Change-Id: Ice5b00bbfffaab61486648dbb01f69bc23c4cb51
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

--- src/tasktree/qtasktree.h
+++ src/tasktree/qtasktree.h
@@ -203,17 +203,10 @@ class Storage final : public StorageBase
 {
 public:
     Storage() : StorageBase(Storage::ctor(), Storage::dtor()) {}
-#if __cplusplus >= 201803L // C++20: Allow pack expansion in lambda init-capture.
     template <typename ...Args>
-    Storage(const Args &...args)
-        : StorageBase([...args = args] { return new StorageStruct{args...}; }, Storage::dtor()) {}
-#else // C++17
-    template <typename ...Args>
-    Storage(const Args &...args)
-        : StorageBase([argsTuple = std::tuple(args...)] {
-            return std::apply([](const Args &...arguments) { return new StorageStruct{arguments...}; }, argsTuple);
-        }, Storage::dtor()) {}
-#endif
+    explicit Storage(const Args &...args)
+        : StorageBase([=] { return new StorageStruct{args...}; }, Storage::dtor()) {}
+
     StorageStruct &operator*() const noexcept { return *activeStorage(); }
     StorageStruct *operator->() const noexcept { return activeStorage(); }
     StorageStruct *activeStorage() const {
