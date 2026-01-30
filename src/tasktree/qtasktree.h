edaddb6fa532a6ee9f22c50c7ab351754090969a
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-01-30 08:58:02 +0100

    QUIP-23: mark all source files with default security header
    Qt TaskTree code doesn't cross any trust boundaries, and the probability
    that any of this code represents an exploitable security vulnerability
    is low.
    
    Task-number: QTBUG-135177
    Change-Id: I262af5bc16dfd76466febcaedf2c0c6b91eca344
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>
    (cherry picked from commit 50af9215f44c233ad8fef4ca0b719e2ca389381f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/tasktree/qtasktree.h
+++ src/tasktree/qtasktree.h
@@ -1,6 +1,8 @@
 // Copyright (C) 2025 Jarek Kobus
 // Copyright (C) 2025 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
 
 #ifndef QTASKTREE_H
 #define QTASKTREE_H
